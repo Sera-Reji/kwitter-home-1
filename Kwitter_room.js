c89ade4ba71ad0af8d904d5b9634b39cb72a6e29
Sera-Reji <81759303+Sera-Reji@users.noreply.github.com>
2021-11-16 10:45:03 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBXXPZ0sNAbC6OEuaUB701qXQyCLsdPCaU",
+    authDomain: "kwitter-a7973.firebaseapp.com",
+    projectId: "kwitter-a7973",
+    storageBucket: "kwitter-a7973.appspot.com",
+    messagingSenderId: "221940975763",
+    appId: "1:221940975763:web:493f8f612068bbf44fa566"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
